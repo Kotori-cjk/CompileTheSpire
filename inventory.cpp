3667fd913ae0c4d3837a0842b6a3a22ca57e2930
张然博 <2549847934@qq.com>
2026-05-29 22:13:09 +0800

    feat: inventory

--- inventory.cpp
+++ inventory.cpp
@@ -1,111 +1,118 @@
 #include "inventory.h"
 
-// --- Code blocks (bag) ---
+//---init---
 
-int Inventory::capacity() const
-{
+Inventory::Inventory(const LevelData* ptr){
+    if(ptr == nullptr){
+        return;
+    }
+    m_capacity = ptr->bagSize;
+    bagBlocks = QVector<CodeBlock>();
+    for(auto it = ptr->chests.constBegin(); it != ptr->chests.constEnd(); ++it){
+        auto chestId = it.key();
+        auto chest = it.value();
+        leftBlocks[chestId] = QSet<CodeBlock>();
+        for(auto it2 = chest.blocks.constBegin(); it2 != chest.blocks.constEnd(); ++it2){
+            leftBlocks[chestId].insert(it2.value());
+        }
+    }
+}
+
+//---bag state---
+
+int Inventory::bagCapacity()const {
     return m_capacity;
 }
 
-void Inventory::setCapacity(int capacity)
-{
-    m_capacity = capacity;
+int Inventory::bagSize()const {
+    return bagBlocks.size();
 }
 
-int Inventory::size() const
-{
-    return m_blocks.size();
+bool Inventory::bagIsEmpty()const {
+    return bagBlocks.isEmpty();
 }
 
-bool Inventory::isEmpty() const
-{
-    return m_blocks.isEmpty();
+bool Inventory::bagIsFull()const {
+    return m_capacity <= bagBlocks.size();
 }
 
-bool Inventory::isFull() const
-{
-    return m_capacity > 0 && m_blocks.size() >= m_capacity;
-}
-
-bool Inventory::add(const CodeBlock &block)
-{
-    if (isFull()) {
+bool Inventory::bagAdd(const CodeBlock &block) {
+    if(bagIsFull()){
         return false;
     }
-    m_blocks.append(block);
+    bagBlocks.append(block);
     return true;
 }
 
-bool Inventory::remove(const QString &blockId)
-{
-    for (int i = 0; i < m_blocks.size(); ++i) {
-        if (m_blocks[i].blockId == blockId) {
-            m_blocks.removeAt(i);
+bool Inventory::bagRemove(const QString &blockId){
+    for(int i = 0; i < bagBlocks.size(); ++i){
+        if(bagBlocks[i].blockId == blockId){
+            bagBlocks.removeAt(i);
             return true;
         }
     }
     return false;
 }
 
-bool Inventory::contains(const QString &blockId) const
-{
-    for (const CodeBlock &block : m_blocks) {
-        if (block.blockId == blockId) {
+bool Inventory::bagContains(const QString &blockId)const {
+    for(const CodeBlock &block : bagBlocks){
+        if(block.blockId == blockId){
             return true;
         }
     }
     return false;
 }
 
-CodeBlock Inventory::get(const QString &blockId) const
-{
-    for (const CodeBlock &block : m_blocks) {
-        if (block.blockId == blockId) {
-            return block;
+QVector<CodeBlock> Inventory::bag()const {
+    return bagBlocks;
+}
+
+QStringList Inventory::bagIds()const {
+    QStringList ids;
+    for(const auto& block : bagBlocks){
+        ids.append(block.blockId);
+    }
+    return ids;
+}
+
+void Inventory::clearBag() {
+    bagBlocks.clear();
+}
+
+//---chest state---
+
+bool Inventory::addBlockFromChest(const QString &blockId, const QString &chestId, QString* errorMsg) {
+    if(bagIsFull()){
+        if(errorMsg != nullptr){
+            *errorMsg = "Bag is full";
+        }
+        return false;
+    }
+    for(const auto &block : leftBlocks[chestId]){
+        if(block.blockId == blockId){
+            bagAdd(block);
+            leftBlocks[chestId].remove(block);
+            return true;
         }
     }
-    return {};
-}
-
-QVector<CodeBlock> Inventory::all() const
-{
-    return m_blocks;
-}
-
-QStringList Inventory::ids() const
-{
-    QStringList result;
-    for (const CodeBlock &block : m_blocks) {
-        result.append(block.blockId);
+    if(errorMsg != nullptr){
+        *errorMsg = "Block is not in chest";
     }
-    return result;
+    return false;
 }
 
-void Inventory::clearBlocks()
-{
-    m_blocks.clear();
-}
-
-// --- Chest state ---
-
-bool Inventory::addBlockFromChest(const QString &blockId, const QString &chestId, QString* errorMsg)
-{
-    bool success = add(this->get(blockId));
-    if(success == false){
-        if(errorMsg!=nullptr)*errorMsg="Bag is full";
-        return false;
-
-    }
-    if(this->chestIsEmpty(chestId)){
-        if(errorMsg!=nullptr)*errorMsg="Chest is empty";
-        return false;
-    }
-    leftBlocks[chestId].remove(blockId);
-    return true;
-}
-
-bool Inventory::chestIsEmpty(const QString &chestId) const
-{
+bool Inventory::chestIsEmpty(const QString &chestId)const {
     return leftBlocks[chestId].empty();
 }
 
+QSet<CodeBlock> Inventory::blocksRemaining(const QString &chestId) {
+    return leftBlocks[chestId];
+}
+
+QStringList Inventory::remainingIds(const QString &chestId){
+    QStringList ids;
+    for(const auto &block : leftBlocks[chestId]){
+        ids.append(block.blockId);
+    }
+    return ids;
+}
